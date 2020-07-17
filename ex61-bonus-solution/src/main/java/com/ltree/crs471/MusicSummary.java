276cd73ca2af409a4166502df27ed5c4e1a39d6b
Ian Darwin <ian@darwinsys.com>
2020-07-17 12:43:41 -0400

    Revise using 2nd rewrite, uses Files.lines()

--- ex61-bonus-solution/src/main/java/com/ltree/crs471/MusicSummary.java
+++ ex61-bonus-solution/src/main/java/com/ltree/crs471/MusicSummary.java
@@ -1,69 +1,79 @@
 package com.ltree.crs471;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Ian's Bonus Solution for Ex61 - Recordings by Category.
- * (might be easier to use "select count categorized" in SQL :-)).
+ * (might be easier to use "select count categorized" in SQL,
+ * but if we'd used a relational database, this example
+ * would not be needed :-)).
  */
 public class MusicSummary {
-	public static void main(String[] args) {
-		final String INFILE = "music.db";
-		final String OUTFILE = "musicsummary.txt";
-
-		//
-		// Complete the steps below:
-		//
-
-		int recordingCount = 0;
-
-		// Step 1. Create a BufferedReader that reads from file "music.db"
-		// Hint: you will need to create a FileReader also.
-		// Don't forget to handle exceptions!
-
-		// Map from category name to count
-		Map<String, Integer> map = new HashMap<>();
-
-		try (BufferedReader in = new BufferedReader(new FileReader(INFILE));
-				PrintWriter pw = new PrintWriter(new File("results.text"));) {
-
-			// Step 2. Read each line in the file, until end of file
+	final static String INFILE = "music.db";
+	final static String OUTFILE = "musicsummary.txt";
+	static final String SEPARATOR = "----------------------";
+	static int recordingCount;
+	// Map from category name to count (for bonus)
+	final static Map<String, Integer> categoriesCount = new HashMap<>();
+	
+	public static void main(String[] args) throws Exception {
+		
+		// Step 1. Read the file "music.db" a line at a time.
+		// Step 2. Read each line
+		// Step 3. Count. This version calls doLine(line) on each line.
+		
+		// Steps 1 & 2 - Traditional way
+		recordingCount = 0; categoriesCount.clear();
+		try (BufferedReader is = new BufferedReader(new FileReader(INFILE))) {
 			String line;
-			boolean atStart = true;	// First recording lacks "----" so start true.
-
-			while ((line = in.readLine()) != null) {
-
-				if (atStart) {	// At start of a recording?
-					String[] split = line.split(", ?");
-					String category = split[2];
-					int n = map.getOrDefault(category, 0);
-					map.put(category, ++n);
-					atStart = false;
-				}
-
-				// Step 3. Increment recordingCount when the recording separator
-				if ("----------------------".equals(line)) {
-					atStart = true;
-					++recordingCount;
-				}
+			while ((line = is.readLine()) != null) {
+				doLine(line);
 			}
+		}
 
-			// Step 4. Print the number of recordings found in each category, and total.
-			for (String k : map.keySet()) {
-				System.out.printf("Category %s count %d%n", k, map.get(k));
+		// Steps 1 & 2 - Modern, Best practices way (a bit shorter, eh?)
+		recordingCount = 0; categoriesCount.clear();
+		Files.lines(Path.of(INFILE)).forEach(line -> doLine(line));
+		
+		// Bonus step: summarize categories
+		for (String cat : categoriesCount.keySet()) {
+			System.out.printf("Category %s has %d recordings\n", 
+					cat, categoriesCount.get(cat));
+		}
+		
+		// Step 4 - print total number of recordings to console
+		var message = String.format("Total %d recordings", recordingCount);
+		System.out.println(message);
+		// And to our output file
+		try (PrintWriter pw = new PrintWriter(OUTFILE);) {
+			pw.println(message);
+		}
+	}
+
+	private static boolean atStart = false;
+
+	private static void doLine(String line) {
+		// Step 3. Increment recordingCount when the recording separator found
+		if (SEPARATOR.equals(line)) {
+			atStart = true;
+			++recordingCount;
+		} else {
+			if (atStart) {
+				String[] fields = line.split(", *");
+				System.out.printf("Found %d fields, first = %s\n", fields.length, fields[0]);
+				String category = fields[2];
+				categoriesCount.put(category, 
+						categoriesCount.getOrDefault(category, 0) + 1);
 			}
-			System.out.println("Total recordings: " + recordingCount);
-
-			// MAJOR STEP 2 - output file (opened in try-with-resources above)
-			pw.println(recordingCount);
-		} catch (IOException e) {
-			System.out.println(e);
+			atStart = false;
 		}
 	}
 }
+
+
