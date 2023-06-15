265dd397f3c33bf62e75c4d0adef5ecc1f4ba14f
Ian Darwin <ian@darwinsys.com>
2023-06-15 08:02:00 -0400

    Expand comment, no code change

--- junitdemo/src/main/java/com/iandarwinphoto/FrameSize.java
+++ junitdemo/src/main/java/com/iandarwinphoto/FrameSize.java
@@ -2,7 +2,9 @@ package com.iandarwinphoto;
 
 public class FrameSize {
 
-	/** Given a width, compute the height, assuming 8/10 proportion */
+	/** Given a width, compute the height, assuming 8/10 proportion
+	 * common in photo prints, eg., 4x5, 8/10, etc
+	 */
 	public static int getHeight(int width) {
 		return (width * 8)/10;
 	}
