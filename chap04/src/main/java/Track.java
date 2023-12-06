4d534c4a37a15d8f1c0132439d428b419f165cab
Ian Darwin <ian@darwinsys.com>
2023-12-05 20:11:06 -0500

    typo fixes

--- chap04/src/main/java/Track.java
+++ chap04/src/main/java/Track.java
@@ -1,14 +1,14 @@
 public record Track(String title, Duration runningTime) {
 
     // The "get" methods are named title() not getTitle(), etc.
-    // If you need the old-style names for compabibily, just write
+    // If you need the old-style names for compatibility, just write
     // trivial delegation methods:
     public String getTitle() {
         return title;
     }
 
     // And, to add to our woes, musicians and/or record companies sometimes
-    // have to change names to reflect modern sensibilities, hend the "with" pattern
+    // have to change names to reflect modern sensibilities, hence the "with" pattern
     Track withTitle(String newTitle) {
         return new Track(newTitle, runningTime());
     }
