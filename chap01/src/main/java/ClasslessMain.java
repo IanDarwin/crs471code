77b109e9a449c7fb3dc1f35f5c234448f4820036
Ian Darwin <ian@darwinsys.com>
2023-12-05 20:15:27 -0500

    Another quick demo

---
+++ chap01/src/main/java/ClasslessMain.java
@@ -0,0 +1,6 @@
+// "Classless main program" from "JEP445:
+// Unnamed Classes and Instance Main Methods"
+// No class needed, no public static needed, no String[] needed
+void main() {
+	System.out.println("Hello from Java 21!");
+}
