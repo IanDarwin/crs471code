809c893784d2e8326871e7af70cecbd957e4bfe5
Ian Darwin <ian@darwinsys.com>
2023-12-13 13:30:36 -0500

    Make work on older Java, add comments

--- chap03/src/main/java/TwoDimArray.java
+++ chap03/src/main/java/TwoDimArray.java
@@ -1,14 +1,36 @@
-void main() {
+/**
+ * Two-D arrays, created using array initializer,
+ * iterated two ways.
+ */
+public class TwoDimArray {
 
-	//int[][] data = new int[4][3];
-	int[][] data = {
-		{ 1, 2, 3 },
-		{ 4, 5, 6, 7}
-	};
+	public static void main(String[] uselessArgs) {
 
-	for (int i = 0; i < data.length; i++) {
-		for (int j = 0; j < data[i].length; j++) {
-			System.out.println(data[i][j]);
+		// Start of Hard way to populate
+		//int[][] data = new int[4][3];
+		
+		// Better, easy way to populate
+		// using Java "array initializer" syntax
+		int[][] data = {
+				{ 1, 2, 3 },
+				{ 4, 5, 6, 7},
+				{ 42 },
+		};
+
+		System.out.println("Indexed iteration");
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < data[i].length; j++) {
+				System.out.println(data[i][j]);
+			}
 		}
+		
+		// Use this form if index isn't needed in loop
+		System.out.println("Easier iteration");
+		for (int[] row : data) {
+			for (int num : row) {
+				System.out.println(num);
+			}
+		}
+
 	}
 }
